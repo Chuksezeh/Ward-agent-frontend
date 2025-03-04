77cdb8fe68b5d118770b581c1b3c574f8dd88135
Chukwuka Paul Ezeh <chuksintellectual@gmail.com>
2025-03-04 16:33:02 +0100

    my

--- src/layout/navBar.js
+++ src/layout/navBar.js
@@ -1,6 +1,10 @@
-import { useState } from "react";
+
 import "./navbar.css"
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import { FiArrowRight } from "react-icons/fi";
+
 
 const NavBar = (()=>{
 
@@ -9,24 +13,56 @@ const NavBar = (()=>{
     const toggleNav = () => {
       setIsNavActive(!isNavActive);
     };
+
+const userInfo = localStorage.getItem("agentUserInfo");
+const userData = JSON.parse(userInfo);
+
+const handleLogOut = (()=>{
+    localStorage.removeItem("agentUserInfo")
+})
+
+const [menuActive, setMenuActive] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolling(window.scrollY >= 85);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  
     return(
 
 
         <>
-        <nav>
+     <nav>
        <Link to="*"  style={{cursor:"pointer", textDecoration:"none"}}> <div className="logo">  WARD AGENT  </div></Link> 
         <input type="checkbox" id="checkbox"/>
         <label for="checkbox" id="icon">
             <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
         </label>
         <ul>
-            <li><a href="#" className="active">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><Link to="/user-registration">Register</Link> </li>
-            <li><a href="#">Blog</a></li>
+            <li style={{cursor:"pointer"}}><Link to="*"><a  className="">Home</a> </Link></li>
+            {/* <li><a >About</a></li> */}
+            {
+                !userData ?  <li><Link to="/user-registration">Register</Link> </li>:  <li onClick={handleLogOut}><Link to="*">Logout</Link> </li>
+            }
+
+            {
+                userData &&  <li ><Link to="user-profile">My Account</Link> </li>
+            }
+           
+         
             <li><a href="#">Contact</a></li>
         </ul>
-    </nav>
+    </nav> 
+
+
+    
+
         
         
         </>
--- src/layout/userLayout/userLayout.js
+++ src/layout/userLayout/userLayout.js
@@ -1,22 +1,55 @@
-import NavBar from "../navBar"
+import { useEffect } from "react";
+import NavBar from "../navBar";
+import PersonalInfoCards from "./personalInfoCards";
+import "./userLayout.css";
+import { useNavigate } from "react-router-dom";
 
 
 
 const UserLayout = (()=>{
 
+    const userInfo = localStorage.getItem("agentUserInfo");
+    const userData = JSON.parse(userInfo);
 
+    const navigate = useNavigate();
+
+    console.log("userData >>>", userData )
+
+    useEffect(()=>{
+     if(!userData)
+      navigate("*")
+    },[userData])
 
     return(
 
         <>
 
         <NavBar/>
-        <div>
-
-
-
+        <div className="control-all">
+        <div className="control1 container">
+            <div className="control-inner">
+             <span style={{color:"white"}}> Now Open</span>
+           <div  className="inf-control">
+            <h1 className="incfav">Unlock Information, Stay Connected – Your Community, Your Access!</h1>
+            <p className="incfavP">Welcome to <span style={{color:"#0075FF", fontWeight:"bold"}}>Ward Agent</span>, where access to information is tailored to your community level. From Kindred to State, your subscription ensures you see only what matters to you. With secure, structured access and admin-managed data, 
+                we empower you to stay informed while keeping information safe and organized.</p>
+          </div>
+          <div>
+            <span className="focuStext">Your Access, Your Community, Your Control.</span>
+          </div>
+          </div>
+        </div>  
+      
             
         </div>
+
+       <PersonalInfoCards/>
+  
+
+  
+
+
+        
         
         </>
     )
